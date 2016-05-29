ec1821356f559711f49eb334babe9ae5e8065481
NoxErinys <noxerinys@gmail.com>
2016-05-29 20:44:14 +0300

    +MobScript

---
+++ Assets/Scripts/Mob/MobScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
